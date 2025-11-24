bfeb34383d573b1d89d98f90adc0c7348d551870
Ángel Díaz <ad9311@proton.me>
2025-11-24 12:11:29 -0500

    Update .rubocop.yml
    Fix rubocop issues
    Install annotaterb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,11 @@
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  description :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Task < ApplicationRecord
 end
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,4 +1,13 @@
-require "test_helper"
+# == Schema Information
+#
+# Table name: tasks
+#
+#  id          :integer          not null, primary key
+#  description :string           not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
   # test "the truth" do
